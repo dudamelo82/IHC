2bad8ce1101b5546ad50e1eef2d5c695f04a64b0
dudamelo82 <43159230+dudamelo82@users.noreply.github.com>
2020-03-13 21:47:48 -0300

    Add files via upload

---
+++ Util.js
@@ -0,0 +1,14 @@
+/*CRUD*/
+/* Método Adicionar*/
+
+function Adicionar(pessoa, n) {
+    var pessoa = [];
+
+    for (i = 0; i < n; i++) {
+        if (pessoa != null) {
+            pessoa++;
+        }
+
+    };
+
+}
